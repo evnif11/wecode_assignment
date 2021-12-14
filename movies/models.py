9746bd3b1548084201b3d5bab10a0eef3bdc0efd
Chanjoo lee <dlckswn14@gmail.com>
2021-12-14 17:10:47 +0900

    데이터베이스 모델링 movie, actor 클래스 추가

---
+++ movies/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    date_of_birth = models.DateField()
+
+    def __str__(self) -> str:
+        return self.last_name + self.first_name
+
+    class Meta:
+        db_table = "actors"
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    release_date = models.DateField()
+    running_time = models.IntegerField()
+    actor = models.ManyToManyField(Actor)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        db_table = 'movies'
