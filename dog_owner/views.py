81dbdc3f7d925d7855c133023319c687b625a110
Chanjoo lee <dlckswn14@gmail.com>
2021-12-14 15:41:35 +0900

    dog, owner post api 추가

--- dog_owner/views.py
+++ dog_owner/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+
+from dog_owner.models import Owner, Dog
+
+class OwnerView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        Owner.objects.create(
+            name = data['name'],
+            email = data['email'],
+            age = data['age']
+        )
+        return JsonResponse({'message':'created'}, status=201)
+
+
+class DogView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        Dog.objects.create(
+            name = data['name'],
+            age = data['age'],
+            owner = Owner.objects.get(id=data['owner_id'])
+        )
+        return JsonResponse({'message':'created'}, status=201)
